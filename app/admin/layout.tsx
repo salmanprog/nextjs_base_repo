643e064be93f37d1f404f2e522b719145a3fa1ca
arsalanahmeddev1 <arsalanahmeddev1@gmail.com>
2025-11-14 04:55:04 +0500

    minor changes in admin dashboard

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -35,7 +35,7 @@ export default function AdminLayout({
         className={`flex-1 transition-all duration-300 ease-in-out ${mainContentMargin}`}
       >
         <AppHeader />
-        <div className="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">{children}</div>
+        <div className="p-4 mx-auto  md:p-6">{children}</div>
       </div>
     </div>
     </UserProvider>
